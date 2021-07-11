31c09eb9d0f57393abb235d761e69a97635b9f95
Bhupender Rawat <bhupenderrawat742@gmail.com>
2021-07-11 11:51:25 +0530

    added validation to the email and password fields

--- LoginPage/ViewController.swift
+++ LoginPage/ViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var CodeplayonLabel: UILabel!
     
-    @IBOutlet weak var LogInButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     @IBAction func ForgetPasswordButton(_ sender: Any) {
         print("Forget Password Button pressed.")
@@ -19,8 +19,61 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        logInButton.isEnabled = false
+        logInButton.backgroundColor = UIColor.gray
+        
+        self.emailTextField.addTarget(self, action: #selector(textFieldEditingChanged), for: .editingChanged)
+        
+        self.passwordTextField.delegate = self
+        self.passwordTextField.addTarget(self, action: #selector(textFieldEditingChanged), for: .editingChanged)
+    }
+    
+    @objc private func textFieldEditingChanged() {
+        guard let email = emailTextField?.text else { return }
+        guard let password = passwordTextField?.text else { return }
+        
+        if email.validateEmail() && password.validatePassword() {
+            logInButton.isEnabled = true
+            logInButton.backgroundColor = UIColor.blue
+        } else {
+            logInButton.isEnabled = false
+            logInButton.backgroundColor = UIColor.gray
+        }
+    }
+}
+
+extension String {
+    
+    func validateEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        return applyPredicateOnRegex(regexStr: emailRegEx)
+    }
+    
+    func validatePassword() -> Bool {
+        let passRegEx = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&#])[A-Za-z\\d$@$!%*?&#]{8,}"
+
+        return applyPredicateOnRegex(regexStr: passRegEx)
+    }
+    
+    func applyPredicateOnRegex(regexStr: String) -> Bool {
+        let trimmedString = self.trimmingCharacters(in: .whitespaces)
+        let validateString = NSPredicate(format: "SELF MATCHES %@", regexStr)
+        let isValidString = validateString.evaluate(with: trimmedString)
+        return isValidString
+    }
+}
+
+//MARK:- UITextFieldDelegate
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let maxLength = 15
+        let currentString: NSString = (textField.text ?? "") as NSString
+        let newString: NSString = currentString.replacingCharacters(in: range, with: string) as NSString
+       
+        return newString.length <= maxLength
     }
     
 }
